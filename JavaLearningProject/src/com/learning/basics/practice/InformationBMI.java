05be3127997a619bdd36f8d2adc141070384bcee
Leydi Franco <leydilufranco@gmail.com>
2023-07-18 17:56:07 -0600

    Calculate BMI

---
+++ JavaLearningProject/src/com/learning/basics/practice/InformationBMI.java
@@ -0,0 +1,40 @@
+package com.learning.basics.practice;
+
+import java.sql.SQLOutput;
+
+public class InformationBMI {
+
+    private double weight;
+    private double height;
+    private double result;
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void calculateBMI(){
+
+        result = weight / (height*height);
+
+        if (result < 18.5){
+
+            System.out.println( "Your BMI is Underweight " + result);
+        } else if (result >= 18.5 && result <= 24.9) {
+
+            System.out.println("your BMI is Normal weight " +result);
+
+
+        } else if (result >= 25.0 && result <= 29.0 ) {
+
+            System.out.println(" Your BMI is Overweight " +result );
+
+        } else if (result >= 30.0) {
+
+
+        }
+    }
+}
---
+++ JavaLearningProject/src/com/learning/basics/practice/MainBMI.java
@@ -0,0 +1,13 @@
+package com.learning.basics.practice;
+
+public class MainBMI {
+
+    public static void main(String[] args) {
+        InformationBMI bmi = new InformationBMI();
+
+        bmi.setWeight(84.9);
+        bmi.setWeight(1.78);
+        bmi.calculateBMI();
+
+    }
+}
